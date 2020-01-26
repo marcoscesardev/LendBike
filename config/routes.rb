09e79c37689240daaee32809eb4973f09d9f1fd2
marcos <mcsar1997@gmail.com>
2020-01-26 16:09:47 -0200

    feat: Remove code from template

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :bikes
   devise_for :users, :controllers => { :sessions => "sessions" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'stack#index'
-
-  get ':page' => 'stack#show', as: 'stack'
+  
+  root to: 'home#index'
 end
