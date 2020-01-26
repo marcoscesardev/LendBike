c879556d9aaf22020c9aaa94329ebf8087a2d90b
marcos <mcsar1997@gmail.com>
2020-01-26 18:46:24 -0200

    feat: Build the base architecture for CRUD

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,2 +1,15 @@
-class BikesController < ApplicationController
+class BikesController < CrudController
+  before_action :load_bike_situations, only: [:new, :create, :edit, :update]
+  
+  private
+
+  def load_bike_situations
+    @bike_situations = Bike.translated_situations.reject{|a| a.include?('in_use') }
+  end
+
+  protected
+
+  def object_params
+    params.require(:bike).permit(:situation, :code, :active)
+  end
 end
---
+++ app/controllers/crud_controller.rb
@@ -0,0 +1,51 @@
+class CrudController < ApplicationController
+  inherit_resources
+
+  def index
+    build_resource
+
+    respond_to do |format|
+      format.html { render :index }
+    end
+  end
+
+  def create
+    @object = resource_class.new(object_params)
+
+    if @object.save 
+      flash[:notice] = I18n.t(
+          'messages.object_successfully_created',
+          object_name: resource_class.model_name.human
+        )
+
+      redirect_to resource_path(@object) 
+    else
+      flash[:alert] = @object.errors if @object.errors.present?
+
+      render :new
+    end
+  end
+
+  def update
+    @object = resource_class.find(params[:id])
+    
+    if @object.update(object_params)
+      flash[:notice] = I18n.t(
+          'messages.object_successfully_updated',
+          object_name: resource_class.model_name.human
+        )
+
+      redirect_to resource_path(@object) 
+    else
+      flash[:alert] = @object.errors if @object.errors.present?
+
+      render :edit
+    end
+  end
+
+  protected
+
+  def build_resource
+    resource_class.new
+  end
+end
