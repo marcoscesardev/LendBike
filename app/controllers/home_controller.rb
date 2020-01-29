887acbdd16bae038142a3b71d33fd40e4707f7eb
Marcos César <mcsar1997@gmail.com>
2020-01-29 18:03:35 -0300

    style: Add dashboard page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,12 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @lends_vacancies = Lend.current.group_by(&:station).map do |station, lend|
+        { 
+          station: station.name,
+          vacancies_percentage: ((lend.size.to_f/station.vacancies) * 100).round,
+          lend_size: lend.size,
+          station_vacancies: station.vacancies
+        }
+      end.sort_by{ |station| station[:vacancies_percentage] }.first(5)
+  end
 end
